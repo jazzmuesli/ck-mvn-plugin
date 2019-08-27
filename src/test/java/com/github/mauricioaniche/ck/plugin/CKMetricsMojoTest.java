3aa0460727e7d9966648f8c602f1cc14e4bac2c4
preich <preich@192.168.1.101>
2019-08-27 21:15:56 +0100

    added checks for method modifiers

--- src/test/java/com/github/mauricioaniche/ck/plugin/CKMetricsMojoTest.java
+++ src/test/java/com/github/mauricioaniche/ck/plugin/CKMetricsMojoTest.java
@@ -5,8 +5,12 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Modifier;
 import java.nio.charset.Charset;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.apache.commons.csv.CSVFormat;
@@ -16,6 +20,7 @@ import org.apache.maven.plugin.testing.MojoRule;
 import org.apache.maven.project.MavenProject;
 import org.junit.Rule;
 import org.junit.Test;
+
 
 /**
  * integration test for CKMetricsMojo.
@@ -41,21 +46,38 @@ public class CKMetricsMojoTest {
 		MavenProject project = (MavenProject) rule.getVariableValueFromObject(myMojo, "project");
 		List<String> testDirs = project.getTestCompileSourceRoots();
 		assertEquals(1, testDirs.size());
-		String classFileName = testDirs.get(0) + "/class.csv";
-		assertTrue("Unexpected class.csv filename: " + classFileName,
-				classFileName.endsWith("src/test/java/class.csv"));
+		List<CSVRecord> testClassMetrics = getMetrics(testDirs, "class.csv");
+		Optional<CSVRecord> thisClassMetrics = testClassMetrics.
+				stream().
+				filter(p->p.get("class").equals(getClass().getCanonicalName())).
+				findFirst();
+		assertTrue(thisClassMetrics.isPresent());
+		Integer publicMethodsInThisClass = Integer.valueOf(thisClassMetrics.get().get("publicMethods"));
+		assertTrue("At least one method is expected, " + publicMethodsInThisClass + " found", publicMethodsInThisClass >= 1);
+		Integer classModifiers = Integer.valueOf(thisClassMetrics.get().get("modifiers"));
+		assertTrue("This class is expected to be public, but found " + classModifiers, Modifier.isPublic(classModifiers));
 		
-		File file = new File(classFileName);
+		List<CSVRecord> testMethodMetrics = getMetrics(testDirs, "method.csv");
+		assertTrue("This class is expected to have at least 2 methods, found " + testMethodMetrics.size(), testMethodMetrics.size() >= 2);
+		Map<String, Integer> testMethodModifiers = testMethodMetrics.stream().
+				collect(Collectors.toMap(
+						record -> record.get("method"),
+						record -> Integer.valueOf(record.get("modifiers"))));
+		System.out.println("testMethodModifiers: " + testMethodModifiers);
+		assertTrue("getMetrics should be private", Modifier.isPrivate(testMethodModifiers.get("getMetrics/2[java.util.List<java.lang.String>,java.lang.String]")));
+		assertTrue("testPlugins should be public", Modifier.isPublic(testMethodModifiers.get("testPlugin/0")));
+	}
+
+	private List<CSVRecord> getMetrics(List<String> testDirs, String fname) throws IOException {
+		String fullFileName = testDirs.get(0) + "/" + fname;
+		assertTrue("Unexpected " + fname + " filename: " + fullFileName,
+				fullFileName.endsWith("src/test/java/" + fname));
+		
+		File file = new File(fullFileName);
 		assertTrue("File " + file + " does not exist", file.exists());
 		CSVParser parser = CSVParser.parse(file, Charset.defaultCharset(), CSVFormat.DEFAULT.withFirstRecordAsHeader());
 		List<CSVRecord> allRecords = parser.getRecords();
-		List<CSVRecord> thisClassMetrics = allRecords.
-				stream().
-				filter(p->p.get("class").equals(getClass().getCanonicalName())).
-				collect(Collectors.toList());
-		assertEquals(1, thisClassMetrics.size());
-		Integer publicMethodsInThisClass = Integer.valueOf(thisClassMetrics.get(0).get("publicMethods"));
-		assertTrue("At least one method is expected, " + publicMethodsInThisClass + " found", publicMethodsInThisClass >= 1);
+		return allRecords;
 	}
 
 }
